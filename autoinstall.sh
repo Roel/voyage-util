4e5e6e7305b70843df6a5dafc41dbaa9dfefc777
Roel Huybrechts <mail@rulus.be>
2010-11-20 12:39:33 +0100

    Halt after autoinstall instead of rebooting.
    This ensures we don't have an autoinstall loop.

--- autoinstall.sh
+++ autoinstall.sh
@@ -91,5 +91,4 @@ echo "  If you want to stop, please press Ctrl+C now !!"
 echo "########################################################################"
 sleep 5
 if [ $? != 0 ] ; then exit 1; fi
-reboot
-
+halt
