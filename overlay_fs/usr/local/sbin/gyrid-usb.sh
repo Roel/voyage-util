e347c86494d6247efa2624477b60ca3ca35731d0
Roel Huybrechts <mail@rulus.be>
2010-11-23 14:59:43 +0100

    Fix the merging part of gyrid-usb.

--- overlay_fs/usr/local/sbin/gyrid-usb.sh
+++ overlay_fs/usr/local/sbin/gyrid-usb.sh
@@ -102,14 +102,14 @@ temporary_usb() {
         cp -a $DATADIR/original_logs/var_log/gyrid $DATADIR/merged_logs
 
         #Merge the logs.
-        for i in `ls -1 $DATADIR/merged_logs | grep -E "([0-F][0-F]){5}[0-F][0-F]"`; do
+        for i in `ls -1 $DATADIR/merged_logs | grep -E "([0-9A-F][0-9A-F]){5}[0-9A-F][0-9A-F]"`; do
             cd $DATADIR/merged_logs/$i
-                [ -f *.bz2* ] && bunzip2 *.bz2*
+                [ `ls -1 *.bz2* | wc -l` -gt 0 ] && bunzip2 *.bz2*
 
-                [ -f scan.log.* ] && cat scan.log.* >> scan.log
+                [ `ls -1 scan.log.* | wc -l` -gt 0 ] && cat scan.log.* >> scan.log
                 if [ -f scan.log ]; then
                     sort scan.log > scan_s.log
-                    grep -E "^[0-9]{8}-[0-9]{6}-[A-z]*,([0-F][0-F]:){5}[0-F][0-F],[0-9]*,(in|out|pass)$" scan_s.log > scan.log
+                    grep -aE "^[0-9]{8}-[0-9]{6}-[A-Za-z]*,([0-9A-F][0-9A-F]:){5}[0-9A-F][0-9A-F],[0-9]*,(in|out|pass)$" scan_s.log > scan.log
                     lines_scan=`wc -l < scan.log`
                     uniq_macs=`cat scan.log | awk -F , '{print $2}' | sort | uniq | wc -l`
                     mv scan.log ../`hostname`-$i-scan.log
@@ -118,10 +118,10 @@ temporary_usb() {
                     uniq_macs=0
                 fi
 
-                [ -f rssi.log.* ] && cat rssi.log.* >> rssi.log
+                [ `ls -1 rssi.log.* | wc -l` -gt 0 ] && cat rssi.log.* >> rssi.log
                 if [ -f rssi.log ]; then
                     sort rssi.log > rssi_s.log
-                    grep -E "^[0-9]{8}-[0-9]{6}-[A-z]*,([0-F][0-F]:){5}[0-F][0-F],-?[0-9]+$" rssi_s.log > rssi.log
+                    grep -aE "^[0-9]{8}-[0-9]{6}-[A-Za-z]*,([0-9A-F][0-9A-F]:){5}[0-9A-F][0-9A-F],-?[0-9]+$" rssi_s.log > rssi.log
                     lines_rssi=`wc -l < rssi.log`
                     mv rssi.log ../`hostname`-$i-rssi.log
                 else
